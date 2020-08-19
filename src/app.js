829494f7f82a64095e11ccdb8bd8afaa9ab697c4
Restyled.io <commits@restyled.io>
2020-08-19 16:31:43 +0000

    Restyled by clang-format

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 require('dotenv').config()
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+client.on('ready', () => { console.log(`Logged in as ${client.user.tag}!`); });
 
 client.on('message', msg => {
   if (msg.content === 'ping') {
@@ -11,22 +9,24 @@ client.on('message', msg => {
   }
 });
 
-client.on("voiceStateUpdate", function(oldMember, newMember){
-    const mute_role = process.env.MUTE_CHAT_ROLE || 'talk'
-    if(newMember.channel == undefined){
-        console.log(oldMember.channel.name)
-        let myRole = oldMember.guild.roles.cache.find(role => role.name === mute_role);
-        newMember.member.roles.remove(myRole)
-        //newMember.member.roles.add(newMember.guild.roles.valueOf(role => role.name === "talk"))
-        
-    }
-    else if(oldMember.channel == undefined){
-        console.log(newMember.channel.name)
-        let myRole = oldMember.guild.roles.cache.find(role => role.name === mute_role);
-        newMember.member.roles.add(myRole)
-        //oldMember.member.roles.remove(oldMember.guild.roles.valueOf(role => role.name === "talk"))
-    }
+client.on("voiceStateUpdate", function(oldMember, newMember) {
+  const mute_role = process.env.MUTE_CHAT_ROLE || 'talk'
+  if (newMember.channel == undefined) {
+    console.log(oldMember.channel.name)
+    let myRole =
+        oldMember.guild.roles.cache.find(role => role.name === mute_role);
+    newMember.member.roles.remove(myRole)
+    // newMember.member.roles.add(newMember.guild.roles.valueOf(role =>
+    // role.name === "talk"))
+  }
+  else if (oldMember.channel == undefined) {
+    console.log(newMember.channel.name)
+    let myRole =
+        oldMember.guild.roles.cache.find(role => role.name === mute_role);
+    newMember.member.roles.add(myRole)
+    // oldMember.member.roles.remove(oldMember.guild.roles.valueOf(role =>
+    // role.name === "talk"))
+  }
 });
-
 
 client.login(process.env.TOKEN);
