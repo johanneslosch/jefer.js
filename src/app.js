a85aa46f0d94a994994568a45c8d337e44e387e3
Johannes Losch <hi@johanneslosch.com>
2020-08-20 15:06:21 +0200

    updated :bug:

--- src/app.js
+++ src/app.js
@@ -5,26 +5,16 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
-
 client.on("voiceStateUpdate", function(oldMember, newMember){
     const mute_role = process.env.MUTE_CHAT_ROLE || 'talk'
     if(newMember.channel == undefined){
         console.log(oldMember.channel.name)
-        let myRole = oldMember.guild.roles.cache.find(role => role.name === mute_role);
-        newMember.member.roles.remove(myRole)
-        //newMember.member.roles.add(newMember.guild.roles.valueOf(role => role.name === "talk"))
+        newMember.member.roles.remove(oldMember.guild.roles.cache.find(role => role.name === mute_role))
         
     }
     else if(oldMember.channel == undefined){
         console.log(newMember.channel.name)
-        let myRole = oldMember.guild.roles.cache.find(role => role.name === mute_role);
-        newMember.member.roles.add(myRole)
-        //oldMember.member.roles.remove(oldMember.guild.roles.valueOf(role => role.name === "talk"))
+        newMember.member.roles.add(oldMember.guild.roles.cache.find(role => role.name === mute_role))
     }
 });
 
